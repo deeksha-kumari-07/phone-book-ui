58c22a6833b5ddcef550e79f3689c7c75df5b064
Krishan Kumar <kkumar@catalystone.com>
2023-05-03 10:31:34 +0530

    Improved Header design

--- phone-book/src/header/header.tsx
+++ phone-book/src/header/header.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 export default class Header extends React.Component {
     render() {
         return(
-            <div style={{display: "flex", cursor: "pointer"}}>
+            <div style={{display: "flex", justifyContent: "space-between", margin: "20px", height: "40px"}}>
                 <h2>Phone book</h2>
-                <FilterAltIcon></FilterAltIcon>
-                <AddNewContactButton></AddNewContactButton>
-            </div>
+                <div style={{display: "flex", margin: "5px"}}>
+                    <FilterAltOutlinedIcon style={{backgroundColor: "#DCDCDC", cursor: "pointer", borderRadius: "5px", padding: "5px"}}></FilterAltOutlinedIcon>
+                    <AddNewContactButton></AddNewContactButton>
+                </div>
+            </div> 
         );
     }
 }
@@ -17,7 +19,7 @@ export default class Header extends React.Component {
 class AddNewContactButton extends React.Component {
     render() {
         return(
-            <div>
+            <div style={{backgroundColor: "#90EE90", cursor: "pointer", borderRadius: "5px", padding: "6px", display:"flex", alignItems: "center"}}>
                 <AddCircleOutlineIcon></AddCircleOutlineIcon>
                 <label>New contact</label>
             </div>
